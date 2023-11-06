bacaca010b295ba523a375a1e8963e17586418cd
Nicolás Muros <nicomuros@gmail.com>
2023-11-06 15:49:12 -0300

    first commit

---
+++ backend/src/main/java/com/softtek/m5/ActividadM5Application.java
@@ -0,0 +1,13 @@
+package com.softtek.m5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadM5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActividadM5Application.class, args);
+	}
+
+}
